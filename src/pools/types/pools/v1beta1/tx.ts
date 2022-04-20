9c37014e75ed72167700e5d5749b79aeffb98255
Mikhail Menshenin <mikhail.menshenin@fulldive.com>
2022-04-20 16:17:00 +0500

    remove unnecessary *.js & *.d.ts files

--- src/pools/types/pools/v1beta1/tx.ts
+++ src/pools/types/pools/v1beta1/tx.ts
@@ -2030,9 +2030,3 @@ function longToNumber(long: Long): number {
   }
   return long.toNumber();
 }
-
-// @ts-ignore
-if (util.Long !== Long) {
-  util.Long = Long as any;
-  configure();
-}
---
+++ test/nft/index.ts
@@ -0,0 +1,40 @@
+import { txClient } from '../../lib/nft'
+import { loadWallet } from '../../lib'
+import { expect } from 'chai'
+
+const txAddr = process.env.IMVERSED_TX_ADDR || 'https://tx-endpoint-test.imversed.com:443'
+const qAddr = process.env.IMVERSED_QUERY_ADDR || 'https://query-endpoint-test.imversed.com'
+const mnemonic = process.env.IMVERSED_WALLET_MNEMONIC || 'invalid mnemonic'
+
+describe('NFT module',() => {
+    describe('txs', () => {
+        it('issue denom', async () => {
+            const wallet = await loadWallet(mnemonic)
+            const [account] = await wallet.getAccounts()
+            const tx = await txClient(wallet, { addr: txAddr })
+
+            const msg = tx.msgIssueDenom({
+                id: 'test123',
+                name: 'Test Denom',
+                schema: '',
+                sender: account.address,
+                symbol: 'TST123',
+                mintRestricted: false,
+                updateRestricted: true,
+                oracleUrl: '',
+            })
+
+            const res = await tx.signAndBroadcast([msg], {
+                fee: {
+                    amount: [{
+                        amount: '200',
+                        denom: 'nimv'
+                    }],
+                    gas: '200000'
+                }
+            })
+
+            expect(res.transactionHash).to.not.be.empty
+        })
+    })
+})
