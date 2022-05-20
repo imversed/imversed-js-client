4fc20096f81260144ab1eed5430918d080336bea
Marina Gonokhina <marina.gonokhina@fulldive.com>
2022-05-20 18:40:41 +0300

    wip bank send transaction

--- test/bank/index.ts
+++ test/bank/index.ts
@@ -1,17 +1,48 @@
-import { expect } from 'chai'
-import { bank, loadWallet, createWallet, restoreWallet } from '../../src'
-import { qAddr, mnemonic, denom } from '../lib/env'
+import { expect } from "chai"
+import { bank, loadWallet, createWallet, restoreWallet } from "../../src"
+import { qAddr, mnemonic, denom, address } from "../lib/env"
 
-describe('Bank', () => {
-    it('can query balances', async () => {
-        const wallet = await loadWallet(mnemonic)
-        const [account] = await wallet.getAccounts()
-        const q = await bank.queryClient({ addr: qAddr })
-        const resp =  await q.queryBalance(account.address, denom)
+const { txClient, queryClient } = bank
 
-        expect(resp.data.balance.amount).to.be.eql(parseInt(resp.data.balance.amount))
-        expect(parseInt(resp.data.balance.amount)).to.be.gt(0)
+describe("Bank", () => {
+  it("sends tokens", async () => {
+    const wallet = await loadWallet(mnemonic)
+    const [account] = await wallet.getAccounts()
+
+    const tx = await txClient(wallet, { addr:  address})
+
+    const msg = tx.msgSend({
+      amount: [{
+        amount: "1000",
+        denom
+      }],
+      toAddress: "imv1j7q2njaxtf92e7dnksaa3za9wan2teanqmu07l",
+      fromAddress: account.address
     })
-    it('queries', () => {
+
+    const response = await tx.signAndBroadcast([msg], {
+      fee: {
+        amount: [{
+          amount: "200",
+          denom
+        }],
+        gas: "200000"
+      }
     })
+
+    console.log(response)
+  })
+
+  it("can query balances", async () => {
+    const wallet = await loadWallet(mnemonic)
+    const [account] = await wallet.getAccounts()
+    console.log(account.address)
+    const q = await bank.queryClient({ addr: qAddr })
+    const resp = await q.queryBalance(account.address, denom)
+
+    expect(resp.data.balance.amount).to.be.eql(parseInt(resp.data.balance.amount))
+    expect(parseInt(resp.data.balance.amount)).to.be.gt(0)
+  })
+
+
 })
