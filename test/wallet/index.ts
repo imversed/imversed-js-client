bab19a7218e2e989c500dc54692bac24fe64506c
Denis Esaulov <denis.esaulov@fulldive.com>
2022-06-20 14:33:21 +0300

    add failure test

--- test/wallet/index.ts
+++ test/wallet/index.ts
@@ -1,28 +1,28 @@
-import { expect } from "chai"
+import {expect} from "chai"
 import {convertAddress, loadWallet} from "../../src/"
-import { pubKey, mnemonic, address as addressExpected } from "../utils/env"
+import {pubKey, mnemonic, address as addressExpected} from "../utils/env"
 
-import { toBase64 } from "@imversed/encoding"
+import {toBase64} from "@imversed/encoding"
 
 describe("Wallet", () => {
-    it("can be recovered from mnemonic", async () => {
-        const wallet = await loadWallet(mnemonic)
-        const [account] = await wallet.getAccounts()
+  it("can be recovered from mnemonic", async () => {
+    const wallet = await loadWallet(mnemonic)
+    const [account] = await wallet.getAccounts()
 
-        expect(account.address).to.be.eq(addressExpected)
-        expect(toBase64(account.pubkey)).to.be.eq(pubKey)
-    })
+    expect(account.address).to.be.eq(addressExpected)
+    expect(toBase64(account.pubkey)).to.be.eq(pubKey)
+  })
 })
 
-describe("Converter addresses EVM <=> IMV",  () => {
+describe("Converter addresses EVM <=> IMV", () => {
   const testCases = [
     {
-      imvAddress:"imv1jyc8jnxjeynktsvvpx8uvkpa5hmup0lym2taxg",
-      evmAddress:"0x9130794Cd2C92765c18C098FC6583dA5f7c0bfE4"
+      imvAddress: "imv1jyc8jnxjeynktsvvpx8uvkpa5hmup0lym2taxg",
+      evmAddress: "0x9130794Cd2C92765c18C098FC6583dA5f7c0bfE4"
     },
     {
-      imvAddress:"imv1hhj4v356u4vv30g8779p68c6h9udhwwdrcytrr",
-      evmAddress:"0xBdE556469aE558c8BD07F78A1D1F1Ab978dBb9cd"
+      imvAddress: "imv1hhj4v356u4vv30g8779p68c6h9udhwwdrcytrr",
+      evmAddress: "0xBdE556469aE558c8BD07F78A1D1F1Ab978dBb9cd"
     }
   ]
 
@@ -37,6 +37,19 @@ describe("Converter addresses EVM <=> IMV",  () => {
     testCases.forEach(test => {
       const {imvAddress, evmAddress} = test
       expect(convertAddress(evmAddress)).to.be.eq(imvAddress)
+    })
+  })
+
+  const incorrect = ["emv1jyc8jnxjeynktsvvpx8uvkpa5hmup0lym2taxg", "09130794Cd2C92765c18C098FC6583dA5f7c0bfE4"]
+  incorrect.forEach(address => {
+    it("should convert only support address", () => {
+      try{
+        convertAddress(address)
+        throw new Error("not pass")
+      } catch (e) {
+        expect(e instanceof Error).to.be.true
+        expect(e.message).to.be.eq("unsupported type of address")
+      }
     })
   })
 })
